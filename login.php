d1bf3d63d6ce5966ac3f892c1a7a44d9f66e7266
ahmad0305 <96977081+ahmad0305@users.noreply.github.com>
2023-07-28 23:52:50 +0530

    Add files via upload

---
+++ login.php
@@ -0,0 +1,28 @@
+<?php
+require 'function.php';
+if(isset($_SESSION["id"])){
+  header("Location: index.php");
+}
+?>
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Login</title>
+</head>
+<body>
+   <h2>Login</h2>
+   <form autocomplete="off" action="" method="post">
+      <input type="hidden" id="action" value="login">
+      <label for="">Username</label>
+      <input type="text" id="username" value=""> <br>
+      <label for="">Password</label>
+      <input type="password" id="password" value=""> <br>
+      <button type="button" onclick="submitData();">Login</button>
+    </form>
+
+   <p class="message">Not registered? <a href="register.php">Create an account</a></p>
+   <?php require 'script.php'; ?>
+</body>
+</html>
